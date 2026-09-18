295e1c349b076c3e954dd345804ca6282e18aae7
Lavakeesan <alavakeesan@gmail.com>
2026-09-19 00:57:28 +0530

    Make API and Socket URLs dynamic for production server

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,17 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NEXT_PUBLIC_API_URL?.replace('/api', '') || 'http://localhost:5001';
+const getSocketURL = () => {
+  if (process.env.NEXT_PUBLIC_API_URL) return process.env.NEXT_PUBLIC_API_URL.replace('/api', '');
+  if (typeof window !== 'undefined') {
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+      return 'http://localhost:5001';
+    }
+    return `${window.location.protocol}//${window.location.host}`;
+  }
+  return 'http://localhost:5001';
+};
 
-export const socket = io(URL, {
+export const socket = io(getSocketURL(), {
   autoConnect: false,
   withCredentials: true,
 });
